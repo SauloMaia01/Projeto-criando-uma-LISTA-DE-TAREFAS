9c6dd85691e9dd233da3f8c5bb6149b2c880793b
Saulo Rodrigues Maia <saulormaia@hotmail.com>
2022-04-29 12:58:40 -0300

    Adição de uma tarefa

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const inputElement = document.querySelector(".new-task-input");
 const addTaskButton = document.querySelector(".new-task-button");
 
+const tasksContainer = document.querySelector(".tasks-container");
+
 const validateInput = () => inputElement.value.trim().length > 0;
 
-    /* if (inputElement.ariaValueMax.trim().length > 0) {
+    /* if (inputElement.value.trim().length > 0) {
         return true;
     } else {
         return false;
@@ -14,19 +16,40 @@ const validateInput = () => inputElement.value.trim().length > 0;
 const handleAddTask = () => {
     const inputIsValid = validateInput();
 
+    console.log(inputIsValid);
+
     if (!inputIsValid) {
         return inputElement.classList.add("error");
     }
+
+    const taskItemContainer = document.createElement('div')
+    taskItemContainer.classList.add("task-item")
+
+    const taskContent = document.createElement("p")
+    taskContent.innerText = inputElement.value;
+
+    const deleteItem = document.createElement("i");
+    deleteItem.classList.add("far");
+    deleteItem.classList.add("fa-trash-alt");
+
+    /* appendChild : insere um elemento filho (children) ao elemento pai (parent) na última posição, ela auxilia na criação de um elemento DOM */
+    taskItemContainer.appendChild(taskContent);
+    taskItemContainer.appendChild(deleteItem);
+
+    tasksContainer.appendChild(taskItemContainer);
+
+    inputElement.value = "";
+    
 };
 
 const handleInputChange = () => {
     const inputIsValid = validateInput();
 
-    if (inputIsValid) {
+    if (!inputIsValid) {
         return inputElement.classList.remove("error");
     }
-}
+};
 
 addTaskButton.addEventListener("click", () => handleAddTask());
 
-inputElement.addEventListener('change', () => handleInputChange());
+inputElement.addEventListener("change", () => handleInputChange());
